21934520aacb182e152f44584fe4701a8b1fd67f
thou-vow <thou.vow.etoile@gmail.com>
2025-11-25 23:53:59 -0300

    feat: utilizar mapa de tipos de evento no objeto do barramento

--- microsservicos/barramento/index.js
+++ microsservicos/barramento/index.js
@@ -6,12 +6,14 @@ import axios from 'axios'
 const app = express()
 app.use(express.json())
 
-const eventos = []
+const eventos = {}
 
 //POST /eventos
 app.post('/eventos', async (req, res) => {
   const evento = req.body
-  eventos.push(evento)
+
+  eventos[evento.type] = (eventos[evento.type] || []).push(evento)
+
   console.log(evento)
   try{
     await axios.post('http://localhost:4000/eventos', evento)
